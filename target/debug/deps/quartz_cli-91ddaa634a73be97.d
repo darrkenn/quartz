354b2dd2dbed50efaba22a068757ec9ba2c551fb
darrkenn <darrkenn08@gmail.com>
2025-11-29 23:03:33 +0000

    Setup

---
+++ target/debug/deps/quartz_cli-91ddaa634a73be97.d
@@ -0,0 +1,5 @@
+/mnt/drive/Development/quartz/target/debug/deps/quartz_cli-91ddaa634a73be97.d: quartz-cli/src/main.rs
+
+/mnt/drive/Development/quartz/target/debug/deps/quartz_cli-91ddaa634a73be97: quartz-cli/src/main.rs
+
+quartz-cli/src/main.rs:
---
+++ target/debug/deps/quartz_server-e6d354cab1c4aac3.d
@@ -0,0 +1,5 @@
+/mnt/drive/Development/quartz/target/debug/deps/quartz_server-e6d354cab1c4aac3.d: quartz-server/src/main.rs
+
+/mnt/drive/Development/quartz/target/debug/deps/quartz_server-e6d354cab1c4aac3: quartz-server/src/main.rs
+
+quartz-server/src/main.rs:
